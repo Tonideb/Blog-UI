63717f20216117f4dd22636a98f55fcdc45fcea3
SourceSenseiTheRealOne <source.sensei@proton.me>
2025-05-09 15:28:02 +0100

    udpate 4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // Add custom rules/overrides
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Disable 'any' rule
+      "@next/next/no-img-element": "off", // Disable <img> warning
+    },
+  },
 ];
 
 export default eslintConfig;
